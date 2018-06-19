8ce6a4bd60d648b86bc92f4d8490a82531c3506d
sunghyun <sunghyun1038@naver.com>
2018-06-19 13:51:06 +0900

    add UserModel, IntroActivity

---
+++ app/src/main/java/model/UserModel.java
@@ -0,0 +1,67 @@
+package model;
+
+public class UserModel {
+    private String loginType;
+    private String email;
+    private String uid;
+    private String nickName;
+    private String profile;
+    private String profileThumb;
+    private String createdAt;
+
+    public String getLoginType() {
+        return loginType;
+    }
+
+    public void setLoginType(String loginType) {
+        this.loginType = loginType;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+
+    public String getProfileThumb() {
+        return profileThumb;
+    }
+
+    public void setProfileThumb(String profileThumb) {
+        this.profileThumb = profileThumb;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+}

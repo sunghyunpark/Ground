3a4ef7e23c61666e65a2770ef2e3b45bf4f3366d
sunghyunpark <sunghyun@naver.com>
2018-07-27 13:19:39 +0900

    날씨 url적용

--- app/src/main/java/view/dialog/GroundUtilWeatherDialog.java
+++ app/src/main/java/view/dialog/GroundUtilWeatherDialog.java
@@ -2,19 +2,21 @@ package view.dialog;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Window;
 
 import com.yssh.ground.R;
 
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * 그라운드 유틸 > 날씨 다이얼로그
  */
 public class GroundUtilWeatherDialog extends Dialog {
-    private Context context;
 
     public GroundUtilWeatherDialog(Context context){
         super(context);
@@ -30,5 +32,22 @@ public class GroundUtilWeatherDialog extends Dialog {
         ButterKnife.bind(this);
     }
 
+    @OnClick(R.id.item_1_tv) void item1Btn(){
+        //네이버 날씨
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://weather.naver.com/"));
+        getContext().startActivity(intent);
+    }
+
+    @OnClick(R.id.item_2_tv) void item2Btn(){
+        //기상청
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.kma.go.kr/index.jsp"));
+        getContext().startActivity(intent);
+    }
+
+    @OnClick(R.id.item_3_tv) void item3Btn(){
+        //항공기상청
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://amo.kma.go.kr/new/html/main/main.jsp"));
+        getContext().startActivity(intent);
+    }
 
 }

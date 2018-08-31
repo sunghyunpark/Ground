be094d3c7f652d4de4087195396ff3c5231b8cce
sunghyunpark <sunghyun1038@naver.com>
2018-08-31 16:01:09 +0900

    인트로 백그라운드 이미지 일단 주석처리

--- app/src/main/java/view/IntroActivity.java
+++ app/src/main/java/view/IntroActivity.java
@@ -1,33 +1,26 @@
 package view;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
+import android.support.v7.app.AppCompatActivity;
 import android.view.ViewGroup;
 
-import com.groundmobile.ground.GroundApplication;
 import com.groundmobile.ground.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import util.Util;
 
 public class IntroActivity extends AppCompatActivity {
 
     @BindView(R.id.background_layout) ViewGroup background_vg;
-    private Bitmap resized, bitmap;
+    //private Bitmap resized, bitmap;
 
     @Override
     protected void onDestroy(){
         super.onDestroy();
-        resized.recycle();
-        bitmap.recycle();
+        //resized.recycle();
+        //bitmap.recycle();
     }
 
     @Override
@@ -35,22 +28,22 @@ public class IntroActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
         ButterKnife.bind(this);
-        init();
+        //init();
     }
 
     private void init(){
-        setBackground();
+        //setBackground();
     }
 
     /**
      * Intro 화면의 백그라운드 적용
      */
     private void setBackground(){
-        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.intro_bg_img1);
-        resized = Bitmap.createScaledBitmap(bitmap, GroundApplication.DISPLAY_WIDTH, GroundApplication.DISPLAY_HEIGHT, true);
+        //bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.intro_bg_img1);
+        //resized = Bitmap.createScaledBitmap(bitmap, GroundApplication.DISPLAY_WIDTH, GroundApplication.DISPLAY_HEIGHT, true);
 
-        Drawable d = new BitmapDrawable(getResources(), resized);
-        background_vg.setBackground(d);
+        //Drawable d = new BitmapDrawable(getResources(), resized);
+        //background_vg.setBackground(d);
     }
 
     /**
